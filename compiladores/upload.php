0cb6605c782a3c6e21a71a0a525a6ca62cf16992
Moisito12 <moismm1996@outlook.com>
2020-05-23 14:32:59 -0500

    Upload File

--- compiladores/upload.php
+++ compiladores/upload.php
@@ -5,3 +5,6 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 echo  $target_file;
+$myfile = fopen($target_file, "r") or die("Unable to open file!");
+echo fread($myfile,filesize($target_file));
+fclose($myfile);
