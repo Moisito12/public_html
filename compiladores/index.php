f1e72316210b948b8b3fb24772d53ead82960f55
Moisito12 <moismm1996@outlook.com>
2020-05-23 13:58:24 -0500

    Buttons Bootstrap

--- compiladores/index.php
+++ compiladores/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <title>Compiladores</title>
 </head>
 
@@ -17,14 +18,14 @@
             <td>Tokens</td>
         </tr>
         <tr>
-            <td><textarea name="" style="resize:none" id="" cols="50" rows="20"></textarea></td>
-            <td><textarea name="" style="resize:none" id="" cols="50" rows="20"></textarea></td>
-            <td><textarea name="" style="resize:none" id="" cols="50" rows="20"></textarea></td>
+            <td><textarea name="" style="resize:none" id="" cols="40" rows="15"></textarea></td>
+            <td><textarea name="" style="resize:none" id="" cols="40" rows="15"></textarea></td>
+            <td><textarea name="" style="resize:none" id="" cols="40" rows="15"></textarea></td>
         </tr>
     </table>
-    <button>Cargar Archivo</button>
-    <br>
-    <button>Descarga</button>
+    <button class="btn btn-sm btn-success">Cargar Archivo</button>
+    <br><br>
+    <button class="btn btn-sm btn-primary">Descarga</button>
 </body>
 
 </html>
