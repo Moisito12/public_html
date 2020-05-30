ebfd7a1aba6fb8204b109a5671cd21ba0cce6287
Moisito12 <moismm1996@outlook.com>
2020-05-30 15:25:27 -0500

    Carga Lista

--- compiladores/index.php
+++ compiladores/index.php
@@ -9,6 +9,52 @@
 </head>
 
 <body>
+    <?php
+
+    if (!empty($_POST)) {
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+        // Check if image file is a actual image or fake image
+        /*if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image. 100";
+        $uploadOk = 0;
+    }
+}*/
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Lo siento, el archivo ya existe.";
+            $uploadOk = 0;
+        }
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Lo siento, su archivo es muy grande.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if ($imageFileType != "txt" && $imageFileType != "java") {
+            echo "Lo siento, solo se aceptan archivos txt & java.";
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Lo siento, tu archivo no se puede cargar.";
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Lo siento,hubo un error al cargar el archivo.";
+            }
+        }
+    }
+    ?>
     <h1>Moisés Rentería</h1>
     <h2>Compiladores</h2>
     <table>
@@ -18,7 +64,14 @@
             <td>Tokens</td>
         </tr>
         <tr>
-            <td><textarea name="" style="resize:none" id="" cols="40" rows="15"></textarea></td>
+            <td><textarea name="" style="resize:none" id="" cols="40" rows="15">
+            <?php
+            if (!empty($target_file) && file_exists($target_file)) {
+                $fileContent = file_get_contents($target_file);
+                echo $fileContent;
+                unlink($target_file);
+            }
+            ?></textarea></td>
             <td><textarea name="" style="resize:none" id="" cols="40" rows="15"></textarea></td>
             <td><textarea name="" style="resize:none" id="" cols="40" rows="15"></textarea></td>
         </tr>
@@ -26,7 +79,7 @@
     <button class="btn btn-sm btn-success">Cargar Archivo</button>
     <br><br>
     <button class="btn btn-sm btn-primary">Descarga</button>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <form action="index.php" method="post" enctype="multipart/form-data">
         Select image to upload:
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" value="Subir Archivo" name="submit">
