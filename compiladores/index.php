2ced48779121dcc825f7be8cb6e5c423a06dcdc5
Moisito12 <moismm1996@outlook.com>
2020-06-26 17:21:57 -0500

    Finish commit

--- compiladores/index.php
+++ compiladores/index.php
@@ -2,126 +2,101 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <title>Compiladores</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <title>Compiladores</title>
 </head>
 
 <body>
-    <script>
-        function ejemplo1() {
-            document.getElementById("text_root").innerText = "Mensaje HTML X3";
-        }
+<script src="js/main.js"></script>
+  <?php
 
-        function ejemplo2() {
-            document.getElementById("text_root").innerText = "public class java { public status void main(String[] args) {System.out.println('Hello World'); } }";
-        }
+  if (!empty($_POST)) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-        function tokenizar() {
-            var token = document.getElementById('text_root').value;
-            // var expresionRegular =/[{}() ."!]/ig;
-            var expresionRegular = /\[|\]|\{|\}|\(|\)|\,|\"|\;|\w+|\s|\=/ig;
-            var res = token.match(expresionRegular);
-            var token_espacio = "";
-            for (let i = 0; i < res.length; i++) {
-                //alert(i + " " + res[i]);
-                // token_espacio += res[i] + '\n';
-                var sin_espacio = res[i].trim();
-                if (sin_espacio != "") {
-                    token_espacio += res[i] + '\n';
-                }
-            }
-            document.getElementById('text_token').innerHTML = token_espacio;
-        }
-    </script>
-    <?php
-
-    if (!empty($_POST)) {
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-        // Check if image file is a actual image or fake image
-        /*if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
+    // Check if file already exists
+    if (file_exists($target_file)) {
+      echo "Lo siento, el archivo ya existe.";
+      $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+      echo "Lo siento, su archivo es muy grande.";
+      $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if ($imageFileType != "txt" && $imageFileType != "java") {
+      echo "Lo siento, solo se aceptan archivos txt & java.";
+      $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+      echo "Lo siento, tu archivo no se puede cargar.";
+      // if everything is ok, try to upload file
     } else {
-        echo "File is not an image. 100";
-        $uploadOk = 0;
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo  "<div class='alert alert-success col-md-5 pl-0'  >" . "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded." . "</div>";
+      } else {
+        echo "Lo siento,hubo un error al cargar el archivo.";
+      }
     }
-}*/
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            echo "Lo siento, el archivo ya existe.";
-            $uploadOk = 0;
-        }
-        // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 500000) {
-            echo "Lo siento, su archivo es muy grande.";
-            $uploadOk = 0;
-        }
-        // Allow certain file formats
-        if ($imageFileType != "txt" && $imageFileType != "java") {
-            echo "Lo siento, solo se aceptan archivos txt & java.";
-            $uploadOk = 0;
-        }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Lo siento, tu archivo no se puede cargar.";
-            // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo  "<div class='alert alert-success col-md-5 pl-0'  >" . "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded." . "</div>";
-            } else {
-                echo "Lo siento,hubo un error al cargar el archivo.";
-            }
-        }
-    }
-    ?>
-    <h1>Moisés Rentería</h1>
-    <h2>Compiladores</h2>
-    <table>
-        <tr>
-            <td>Archivo Fuente</td>
-            <td>Análisis Léxico <button class="btn btn-sm btn-warning">Descarga</button> </td>
-            <td>Tokens</td>
-        </tr>
-        <tr>
-            <td><textarea name="text_doc" id="text_root" style="resize:none" id="" cols="40" rows="15">
+  }
+  ?>
+  <h1>Moisés Rentería</h1>
+  <h2>Compiladores</h2>
+  <table>
+    <tr>
+      <td>Archivo Fuente <button onclick="downFuente()" class="btn btn-danger btn-sm">Descarga</button> </td>
+      <td>Análisis Léxico <button onclick="downAnalisis()" class="btn btn-danger btn-sm">Descarga</button> </td>
+
+
+      <td>Tokens <button onclick="downTokens()" class="btn btn-danger btn-sm">Descarga</button> </td>
+    </tr>
+    <tr>
+      <td><textarea name="text_doc" id="text_root" style="resize:none" id="textOne" cols="40" rows="15">
             <?php
             if (!empty($target_file) && file_exists($target_file)) {
-                $fileContent = file_get_contents($target_file);
-                echo $fileContent;
-                unlink($target_file);
+              $fileContent = file_get_contents($target_file);
+              echo $fileContent;
+              unlink($target_file);
             }
             ?></textarea></td>
-            <td><textarea name="text_token" style="resize:none" id="text_token" cols="40" rows="15">
+      <td><textarea name="text_token" style="resize:none" id="text_token" cols="40" rows="15">
             </textarea></td>
-            <td><textarea name="" style="resize:none" id="" cols="40" rows="15"></textarea></td>
-        </tr>
-    </table>
-    <a href="https://warm-mesa-59344.herokuapp.com/compiladores/index.php" class="btn btn-primary btn-sm" target="_blank">Ver en Heroku</a>
-    <a href="https://moisesrenteriacompilador.000webhostapp.com/compiladores/index.php" class="btn btn-danger btn-sm" target="_blank">Ver en 000Webhost</a>
-    <form action="index.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Subir Archivo" name="submit" class="btn btn-sm btn-success">
-    </form>
-    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-        <div class="btn-group mr-2" role="group" aria-label="First group">
-            <input type="button" value="Ejemplo 1" onclick="ejemplo1()" class="btn btn-secondary">
-            <input type="button" value="Ejemplo 2" onclick="ejemplo2()" class="btn btn-secondary">
-        </div>
-        <div class="btn-group mr-2" role="group" aria-label="Second group">
-            <a href="ejemplo1.txt" download="ejemplo1.txt" class="btn btn-secondary">Descarga 1</a>
-            <a href="ejemplo2.txt" download="ejemplo2.txt" class="btn btn-secondary">Descarga 2</a>
-            <a href="ejemplo3.txt" download="ejemplo3.txt" class="btn btn-secondary">Descarga 3</a>
-        </div>
-        <button type="button" class="btn btn-success" onclick="tokenizar()">Tokenizar</button>
-    </div>
-</body>
+      <td><textarea name="acomodado" style="resize:none" id="text3" cols="40" rows="15"></textarea></td>
+    </tr>
+  </table>
+  <a href="http://herokuhernandez.herokuapp.com/compiladores/" class="btn btn-primary btn-sm" target="_blank">Ver en Heroku</a>
+  <a href="https://compiladoreshernandez.000webhostapp.com/compiladores/" class="btn btn-danger btn-sm" target="_blank">Ver en 000Webhost</a>
+  <form action="index.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Subir Archivo" name="submit" class="btn btn-sm btn-success">
+  </form>
 
-</html>
+  <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+    <div class="btn-group mr-2" role="group" aria-label="First group">
+      <input type="button" value="Ejemplo 1" onclick="ejemplo1()" class="btn btn-secondary">
+      <input type="button" value="Ejemplo 2" onclick="ejemplo2()" class="btn btn-secondary">
+    </div>
+
+    <div class="btn-group mr-2" role="group" aria-label="Second group">
+      <a href="ejemplo1.txt" download="ejemplo1.txt" class="btn btn-secondary">Descarga 1</a>
+      <a href="ejemplo2.txt" download="ejemplo2.txt" class="btn btn-secondary">Descarga 2</a>
+      <a href="ejemplo3.txt" download="ejemplo3.txt" class="btn btn-secondary">Descarga 3</a>
+    </div>
+
+    <div class="btn-group mr-2" role="group" aria-label="Second group">
+    <button type="button" class="btn btn-primary btn-sm"  onclick="tokenizar();acomodar()">| Tokenizar |</button>
+    <button type="button" class="btn btn-primary btn-sm" onclick="alerta_tokens()">| Tokens |</button>
+    <button type="button" class="btn btn-primary btn-sm" onclick="alerta()">| Palabras Reservadas |</button>
+    </div>
+
+
+
+  </div>
+</body>
---
+++ compiladores/js/main.js
@@ -0,0 +1,177 @@
+var reservadas = ["abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "default", "do", "double", "else", "enum", "extends", "false", "final", "finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native", "new	null", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "String", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "void", "volatile", "while"]
+
+    var tokens = [ "[", "]","{","}","(",")",";",":","ID","Numero","=",".",","]
+
+    function alerta_tokens() {
+      var index_tokens = "";
+      for (let i = 0; i < tokens.length; i++) {
+        index_tokens += (i + 45) + " " + tokens[i] + "\n";
+      }
+      alert(index_tokens);
+    }
+
+    function alerta() {
+      var index = "";
+      for (let i = 0; i < reservadas.length; i++) {
+        index += (i + 1) + " " + reservadas[i] + "\n";
+      }
+      alert(index);
+      //alert(reservadas.toString().replace(/,/g,"\n"));
+    }
+
+    function ejemplo1() {
+      document.getElementById("text_root").innerText = "Mensaje HTML X3";
+    }
+
+    function ejemplo2() {
+      document.getElementById("text_root").innerText = "public class java { public static void main(String[] args) {System.out.println('Hello World'); } }";
+    }
+
+    function tokenizar() {
+      var token = document.getElementById('text_root').value;
+      var expresionRegular = /\[|\]|{|\}|\(|\)|\,|\:|\w+|\s|\=|\.|\=/ig;
+      var res = token.match(expresionRegular);
+      var token_espacio = "";
+
+      for (let i = 0; i < res.length; i++) {
+
+        var sin_espacios = res[i].trim();
+
+        if (sin_espacios != "") {
+          token_espacio += res[i] + "->Token: " + buscartoken(res[i]) + " ";
+        }
+      }
+      document.getElementById('text_token').innerHTML = token_espacio;
+    }
+
+    function acomodar() {
+      var token = document.getElementById("text_token").value;
+      var expresionRegular = /\[|\]|\{|\}|\(|\)|\,|\;|\"|\=|\.|\w+|\s/ig;
+      var res = token.match(expresionRegular);
+      var token_espacio = "";
+      var contador = 1;
+
+      for (let i = 0; i < res.length; i++) {
+
+        var sin_espacios = res[i].trim();
+        if (sin_espacios != "") {
+          token_espacio += buscartoken2(res[i]) + " ";
+          contador++;
+          if (contador == 10) {
+            token_espacio += "\n";
+            contador = 1;
+          }
+        }
+      }
+      document.getElementById("text3").innerHTML = token_espacio;
+    }
+
+
+    function buscartoken(enumerador) {
+      for (let i = 0; i < reservadas.length; i++) {
+        if (enumerador == reservadas[i]) {
+          return (i + 1) + " Descripcion: Palabras Reservadas" + "\n";
+        }
+      }
+      for (let i = 0; i < tokens.length; i++) {
+
+        switch (enumerador) {
+          case "[":
+          case "]":
+          case "{":
+          case "}":
+          case "(":
+          case ")":
+          case ";":
+          case ":":
+          case "ID":
+          case "Numero":
+          case "=":
+          case ".":
+          case ",":
+            return (i + 45) + " Descripcion: Caracter Especial" + "\n";
+            break;
+          default:
+            if (isNaN(enumerador)) {
+              return 11 + " Descripcion: Identificador" + "\n";
+            } else return 12 + " Descripcion: Numero" + "\n";
+        }
+      }
+      return -1;
+    }
+
+    function buscartoken2(enumerador2) {
+      for (let i = 0; i < reservadas.length; i++) {
+        if (enumerador2 == reservadas[i]) {
+          return (i + 1);
+        }
+      }
+      for (let i = 0; i < tokens.length; i++) {
+
+        switch (enumerador2) {
+          case "[":
+          case "]":
+          case "{":
+          case "}":
+          case "(":
+          case ")":
+          case ";":
+          case ":":
+          case "ID":
+          case "Numero":
+          case "=":
+          case ".":
+          case ",":
+            return (i + 45);
+            break;
+          default:
+            if (isNaN(enumerador2)) {
+              return 11;
+            } else return 12;
+        }
+      }
+      return -1;
+    }
+
+
+    function downAnalisis() {
+      var textToken = document.getElementById("text_token").value;
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(textToken));
+      element.setAttribute('download', "analisis_lexico.txt");
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
+    }
+
+    function downFuente() {
+      var text_root = document.getElementById("text_root").value;
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text_root));
+      element.setAttribute('download', "codigo_fuente.txt");
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
+    }
+
+    function downTokens() {
+      var text3 = document.getElementById("text3").value;
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text3));
+      element.setAttribute('download', "tokens.txt");
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
+    }
